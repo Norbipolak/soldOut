c2d3cbb72ee6013bd2ed413182ad445cd39960a4
Norbipolak <142392455+Norbipolak@users.noreply.github.com>
2023-08-30 13:35:42 +0200

    update

---
+++ data.js
@@ -0,0 +1,106 @@
+let bestSellingAlbums = [
+    {
+        artist: "Michael Jackson",
+        title: "Thriller",
+        year: 1982,
+        genres: ["pop", "post-disco", "funk", "rock"],
+        sale: 70000000,
+    },
+    {
+        artist: "AC/DC",
+        title: "Back in Black",
+        year: 1980,
+        genres: ["hard rock"],
+        sale: 50000000,
+    },
+    {
+        artist: "Whitney Houston",
+        title: "The Bodyguard",
+        year: 1992,
+        genres: ["r&b", "soul", "pop", "soundtrack"],
+        sale: 45000000,
+    },
+    {
+        artist: "Pink Floyd",
+        title: "The Dark Side of the Moon",
+        year: 1973,
+        genres: ["progressive rock"],
+        sale: 45000000,
+    },
+    {
+        artist: "Eagles",
+        title: "Their Greatest Hits (1971 - 1975)",
+        year: 1976,
+        genres: ["country rock", "soft rock", "folk rock"],
+        sale: 44000000,
+    },
+    {
+        artist: "Eagles",
+        title: "Hotel California",
+        year: 1976,
+        genres: ["soft rock"],
+        sale: 42000000,
+    },
+    {
+        artist: "Shania Twain",
+        title: "Come On Over",
+        year: 1997,
+        genres: ["country", "pop"],
+        sale: 40000000,
+    },
+    {
+        artist: "Fleetwood Mac",
+        title: "Rumours",
+        year: 1977,
+        genres: ["soft rock"],
+        sale: 40000000,
+    },
+];
+
+// Average sales
+
+let totalRate = 0 
+
+for (const album of bestSellingAlbums) {
+    totalRate += album.sale // rate = rate + variable   -   rate +- variable
+}
+
+const averageRate = totalRate / bestSellingAlbums.length
+console.log(averageRate)
+
+// Average Age 
+
+let totalYear = 0 
+
+for (const album of bestSellingAlbums) {
+    totalYear += album.year 
+}
+
+const averageYear = totalYear / bestSellingAlbums.length
+
+let thisYear = 2023
+
+const averageAge = thisYear - averageYear
+
+console.log(averageAge)
+
+// oldest/newest album
+
+let newestAlbum = bestSellingAlbums[0]
+
+for (const album of bestSellingAlbums) {
+    if (album.year > newestAlbum.year) {
+        newestAlbum = album
+    }
+    }
+console.log(newestAlbum.title)
+
+let oldestAlbum = bestSellingAlbums[0]
+
+for (const album of bestSellingAlbums) {
+    if (album.year < oldestAlbum.year) {
+        oldestAlbum = album
+    }
+    }
+console.log(oldestAlbum.title)
+
